d9b5fe9393d6e037ade7f39effd59044a5e6985a
Daniel <dlf1987@gmail.com>
2023-02-11 13:21:48 -0500

    slim

--- src/components/form/validators.ts
+++ src/components/form/validators.ts
@@ -21,21 +21,6 @@ export function required(): ValidatorFn {
   };
 }
 
-export function isMin(min: number): ValidatorFn {
-  return function (val: any) {
-    if (val >= min) {
-      return null;
-    }
-
-    return {
-      min: {
-        message: `Must be greater than ${min}`,
-        actual: val,
-        min,
-      },
-    };
-  };
-}
 
 export function isMax(max: number): ValidatorFn {
   return function (val: any) {
@@ -48,20 +33,6 @@ export function isMax(max: number): ValidatorFn {
         message: `Must be less than ${max}`,
         actual: val,
         max,
-      },
-    };
-  };
-}
-
-export function isEmail(): ValidatorFn {
-  return function (val: any) {
-    if (val.includes('@')) {
-      return null;
-    }
-
-    return {
-      max: {
-        message: `Email is invalid`,
       },
     };
   };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,122 +1,41 @@
 import { component$, $ } from "@builder.io/qwik";
 import { type DocumentHead } from "@builder.io/qwik-city";
-import {
-  formArray,
-  formControl,
-  useForm,
-} from "~/components/form/form";
+import { useForm } from "~/components/form/form";
 import { FormDebug } from "~/components/form/form-helpers";
-import type { FormArray, FormControl } from '~/components/form/types';
 import { isMax, required } from "~/components/form/validators";
-import PrettyPrint from "~/components/pretty-print/pretty-print";
 
 export default component$(() => {
   console.log("init page component$");
 
-  type TestForm = {
-    age: FormControl<number>;
-    options: FormArray<FormControl<string>>;
-    // subgroup: FormGroup<{
-    //   sub1: FormControl<string>;
-    //   sub2: FormControl<number>;
-    // }>;
-  };
-
-  const validators = $(() => [required(), isMax(3)]);
-
-  const group = useForm<TestForm>({
-    age: formControl({
+  const group = useForm({
+    age: {
       value: 1,
-      validator: validators,
-    }),
-    options: formArray([
-      formControl({
-        value: 1,
-        validator: validators,
-      }),
-      formControl({
-        value: 1,
-      }),
-    ]),
-    // subgroup: formGroup({
-    //   sub1: formControl({
-    //     value: 1,
-    //   }),
-    //   sub2: formControl({
-    //     value: 1,
-    //     validator: validators,
-    //   }),
-    // }),
-  });
-
-  const add = $(() => {
-    group.push(
-      group.controls.options,
-      formControl({
-        value: `opt ${group.controls.options.controls.length + 1}`,
-        validator: $(() => [required(), isMax(3)]),
-      })
-    );
+      // COMMENT THIS LINE OUT TO STOP BUG
+      validator: $(() => [required(), isMax(3)]),
+      valid: true,
+    },
   });
 
   return (
     <div>
       <div class="my-6 flex gap-x-6">
         <div class="w-1/2 border-2 border-blue-300 p-2">
-          Age:
+          Age: <small>(required, max=3)</small>
           <input
             class="form-input"
             type="text"
             value={group.controls.age.value}
             onInput$={(e) => {
-              const val = (e.target as HTMLInputElement).value;
+              const val = +(e.target as HTMLInputElement).value;
               group.setValue(group.controls.age, val);
-              // group.setValue("age", val);
             }}
           />
           <FormDebug ctrl={group.controls.age} />
         </div>
       </div>
 
-      <div class="my-6 mb-2 flex flex-wrap border-2 border-blue-300 p-2">
-        {group.controls.options.controls?.map((control, i) => (
-          <div class="my-1 w-1/2 px-2" key={`opt-${i}`}>
-            Option {i}
-            <input
-              class="form-input"
-              type="text"
-              value={control.value}
-              onInput$={(e) => {
-                const val = (e.target as HTMLInputElement).value;
-                group.setValue(group.controls.options.controls[i], val);
-                // group.setValue(`options.${i}`, val);
-              }}
-            />
-            <FormDebug ctrl={control} />
-          </div>
-        ))}
-      </div>
-
-      <button
-        class="btn my-2 border rounded p-2 bg-gray-200 hover:bg-gray-300"
-        onClick$={() => add()}
-      >
-        Add option
-      </button>
-
-      {group.controls.options.controls[0].value == "111" && <div>match!</div>}
-
-      <div class="m-1 border p-1">
-        Values:
-        <PrettyPrint data={group.value} />
-      </div>
-      <div class="m-1 border p-1">
-        Errors:
-        <PrettyPrint data={group.errors} />
-      </div>
-      {/* <div class="m-1 border p-1">
-        <PrettyPrint data={group.controls} />
-      </div> */}
+      {/* COMMENT THIS LINE OUT TO STOP BUG */}
+      {group.controls.age.value == 2 && <div>match!</div>}
     </div>
   );
 });
